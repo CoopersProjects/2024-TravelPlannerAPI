fc483313cfbdb14490f352144ea132795ea944bd
CoopersProjects <148034604+CoopersProjects@users.noreply.github.com>
2024-03-06 23:43:38 +1100

    Connected to postgres database, cli controller

---
+++ controllers/cli_controller.py
@@ -0,0 +1,36 @@
+from flask import Blueprint
+from init import db, bcrypt
+from models.users import User
+
+
+db_commands = Blueprint('db', __name__)
+
+@db_commands.cli.command('create')
+def create_tables():
+    db.create_all()
+    print("Tables created.")
+
+@db_commands.cli.command('drop')
+def drop_tables():
+    db.drop_all()
+    print("Tables dropped.")
+
+@db_commands.cli.command('seed')
+def seed_tables():
+   users = [
+       User(
+           username="User1",
+           email="user1@email.com",
+           password=bcrypt.generate_password_hash('123456').decode('utf-8')
+       ),
+       User(
+           username="User2",
+           email="user2@email.com",
+           password=bcrypt.generate_password_hash('123456').decode('utf-8')
+       )
+   ]
+
+   db.session.add_all(users)
+   db.session.commit()
+
+   print("Tables seeded.")
--- models/users.py
+++ models/users.py
@@ -9,14 +9,12 @@ class User(db.Model):
     email = db.Column(db.String(100), unique=True, nullable=False)
     password = db.Column(db.String(100), nullable=False)
 
-    trip = db.relationship('Trip', backref='user', lazy=True)
-
     def __repr__(self):
         return '<User %r>' % self.username
     
 class UserSchema(ma.Schema):
     class Meta:
-        fields = ("id", "username", "email")
+        fields = ("id", "username", "email", "password")
     id = fields.Integer(dump_only=True)
     username = fields.String(required=True)
     email = fields.Email(required=True)
